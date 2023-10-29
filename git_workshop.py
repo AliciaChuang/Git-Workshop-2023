293d463808dccef98205c11f39e0b059cb72557c
Alicia <aliciachuang2003@gmail.com>
2023-10-28 18:21:13 -0700

    added code file

---
+++ git_workshop.py
@@ -0,0 +1,22 @@
+import math
+
+# all functions should take in a numerical value and return a numerical value
+# rename your function
+def func1(num_a):
+    # placeholder return, replace with your own function
+    return -1
+
+def func2(num_b):
+    # placeholder return, replace with your own function
+    return -1
+
+# both people should modify this function
+def func3(num_a, num_b):
+    # do something with inputs and print something
+    print("placeholder output")
+
+a = 1
+b = 1
+x = func1(a)
+y = func2(b)
+func3(x, y)
